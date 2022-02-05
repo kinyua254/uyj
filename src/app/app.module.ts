d718109635eac18914473ee4bd063fa9e8eba1d7
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-02-05 05:52:59 +0300

    snv

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {CoursesService} from './courses.service'
+import { CoursesComponent } from './courses.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, CoursesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CoursesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
